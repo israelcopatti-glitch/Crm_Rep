b9ff171514356f7fda9345952869310d116d1433
israelcopatti-glitch <israelcopatti@gmail.com>
2026-01-19 18:57:52 -0300

    db.py

--- db.py
+++ db.py
@@ -1,1 +1,39 @@
 
+import sqlite3
+
+conn = sqlite3.connect("crm.db", check_same_thread=False)
+cur = conn.cursor()
+
+cur.execute("""
+CREATE TABLE IF NOT EXISTS CLIENTES (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    codigo TEXT,
+    nome TEXT
+)
+""")
+
+cur.execute("""
+CREATE TABLE IF NOT EXISTS PEDIDOS (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    cliente_id INTEGER,
+    data TEXT,
+    codigo_prod TEXT,
+    nome_prod TEXT,
+    qtde REAL,
+    preco_unit REAL,
+    valor_total REAL
+)
+""")
+
+cur.execute("""
+CREATE TABLE IF NOT EXISTS OFERTAS (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    codigo_prod TEXT,
+    nome_prod TEXT,
+    preco_pr REAL,
+    validade TEXT,
+    edicao TEXT
+)
+""")
+
+conn.commit()
